a7115b435be8bd94df4195e85887a11d582f29f4
Sean Sartell <ssartell@rightpoint.com>
2022-03-30 10:39:19 -0500

    fix dct for 2d images

--- src/dct.js
+++ src/dct.js
@@ -1,3 +1,5 @@
+import { sum } from "mathjs";
+
 const quantizationTable = [
   [3, 5, 7, 8, 11, 13, 15, 17],
   [5, 7, 9, 11, 13, 15, 17, 19],
@@ -10,6 +12,43 @@ const quantizationTable = [
 ];
 
 export function dct2(N) {
+  const matrix = [];
+  for (let k = 0; k < N; k++) {
+    let row = [];
+    for(let n = 0; n < N; n++) {
+      if (k == 0) {
+        row.push(1);
+      } else {
+        row.push(Math.cos(Math.PI / N * (n + 0.5) * k));
+      }
+    }
+    matrix.push(row);
+  }
+  return matrix;
+};
+
+export function dct2d(x) {
+  const N = x.length;
+  const matrix = [];
+  for (let k1 = 0; k1 < N; k1++) {
+    let row = [];
+    for (let k2 = 0; k2 < N; k2++) {
+      let sum1 = 0;
+      for(let n1 = 0; n1 < N; n1++) {
+        let sum2 = 0;
+        for(let n2 = 0; n2 < N; n2++) {
+          sum2 += x[n1][n2] * Math.cos(Math.PI / N * (n2 + 0.5) * k2);
+        }
+        sum1 += sum2 * Math.cos(Math.PI / N * (n1 + 0.5) * k1);
+      }
+      row.push(sum1);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+};
+
+export function dct2ortho(N) {
   const matrix = [];
   for (let k = 0; k < N; k++) {
     let row = [];
@@ -33,14 +72,35 @@ export function dequantize(M) {
   return M.map((row, i) => row.map((x, j) => x * quantizationTable[i][j]));
 }
 
-// export function dct3(N) {
-//   const matrix = [];
-//   for (let k = 0; k < N; k++) {
-//     const row = [.5];
-//     for(let n = 1; n < N; n++) {
-//       row.push(Math.cos(Math.PI / N * (k + 0.5) * n));
-//     }
-//     matrix.push(row);
-//   }
-//   return multiply(matrix, 2 / N);
-// }
+export function dct3(N) {
+  const matrix = [];
+  for (let k = 0; k < N; k++) {
+    const row = [.5];
+    for(let n = 1; n < N; n++) {
+      row.push((4 / (N * N)) * Math.cos(Math.PI / N * (k + 0.5) * n));
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
+
+export function dct32d(x) {
+  const N = x.length;
+  const matrix = [];
+  for (let k1 = 0; k1 < N; k1++) {
+    let row = [];
+    for (let k2 = 0; k2 < N; k2++) {
+      let sum1 = 0;
+      for(let n1 = 0; n1 < N; n1++) {
+        let sum2 = 0;
+        for(let n2 = 0; n2 < N; n2++) {
+          sum2 += x[n1][n2] * (n2 == 0 ? .5 : Math.cos(Math.PI / N * (k2 + 0.5) * n2));
+        }
+        sum1 += sum2 * (n1 == 0 ? .5 : Math.cos(Math.PI / N * (k1 + 0.5) * n1));
+      }
+      row.push((4 / (N * N)) * sum1);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
-import { dct2, quantize, dequantize } from './dct.js';
+import { dct2, dct2d, dct32d, quantize, dequantize } from './dct.js';
 import { round, multiply, divide, transpose } from 'mathjs';
+import getPixels from 'get-pixels';
 
-const Q = [
-  [3, 5, 7, 8, 11, 13, 15, 17],
-  [5, 7, 9, 11, 13, 15, 17, 19],
-  [7, 9, 11, 13, 15, 17, 19, 21],
-  [9, 11, 13, 15, 17, 19, 21, 23],
-  [11, 13, 15, 17, 19, 21, 23, 25],
-  [13, 15, 17, 19, 21, 23, 25, 27],
-  [15, 17, 19, 21, 23, 25, 27, 29],
-  [17, 19, 21, 23, 25, 27, 29, 31]
+const x = [
+  [1, 0, 1],
+  [0, 1, 0],
+  [1, 0, 1],
 ];
-
-const x = multiply([
-  [1, 1, 1, 1, 1, 1, 1, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 1, 0, 1],
-  [1, .5, 0, 0, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 1, 0, 1],
-  [1, 0, 0, 1, 1, 0, 0, 1],
-  [1, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 1, 1, 1, 1]
-], 255);
 const N = x.length;
 
-const M = dct2(N);
-const Mt = transpose(M);
+const X = dct2d(x);
+console.table(round(X, 3));
 
-const X = multiply(multiply(M, x), Mt);
-console.table(round(X, 5));
-
-const X2 = quantize(X);
-// const X2 = X;
-console.table(round(X2, 5));
-
-const v = multiply(multiply(Mt, dequantize(X2)), M);
-// const v = multiply(multiply(Mt, X2), M);
+const v = dct32d(X);
 console.table(round(v, 3));
+
+// getPixels('C:/Users/ssartell/Downloads/kian-rory-100.png', (err, pixels) => {
+//   let res = [];
+//   for(let x = 0; x < pixels.shape[0]; x++) {
+//     for(let y = 0; y < pixels.shape[1]; y++) {
+//       res.push(pixels.get(x, y, 0));
+//     }
+//   }
+//   debugger;
+// });
+
+
+
+
+
+// const Mt = transpose(M);
+
+// const X = multiply(multiply(M, x), Mt);
+// console.table(round(X, 5));
+
+// const X2 = quantize(X);
+// // const X2 = X;
+// console.table(round(X2, 5));
+
+// const v = multiply(multiply(Mt, dequantize(X2)), M);
+// // const v = multiply(multiply(Mt, X2), M);
+// console.table(round(v, 3));
